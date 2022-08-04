3303b042829497113d69ef5e96d4c96f0a9f1367
Alex <alexgabrielsgg@hotmail.com>
2022-08-03 22:45:15 -0600

    Diseño y botones.

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -46,7 +46,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(114, 373);
+            this.button1.Location = new System.Drawing.Point(120, 372);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
